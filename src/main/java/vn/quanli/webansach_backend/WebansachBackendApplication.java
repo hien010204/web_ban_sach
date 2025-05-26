78ddbe1005e05a0a155b7e587021f4ea4e612e50
BuiXuanHien <155499678+qe180008@users.noreply.github.com>
2025-05-26 11:18:43 +0700

    Initial commit

---
+++ src/main/java/vn/quanli/webansach_backend/WebansachBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.quanli.webansach_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebansachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebansachBackendApplication.class, args);
+	}
+
+}
